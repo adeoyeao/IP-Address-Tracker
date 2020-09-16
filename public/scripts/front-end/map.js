287a7c3b279b1c74f2412bd206ccd77a163c000b
Alfred Adeoye <alfred.adeoye@cantab.net>
2020-09-16 12:03:37 +0100

    update

--- public/scripts/front-end/map.js
+++ public/scripts/front-end/map.js
@@ -1,9 +1,11 @@
+import { API_Key } from "../keys.js"
+
 const map = document.querySelector("#mapid")
 const lat = map.getAttribute("data-lat")
 const lng = map.getAttribute("data-lng")
 
 const mymap = L.map("mapid").setView([lat, lng], 13)
-const APIKey = `pk.eyJ1IjoiYWRlb3llYW8iLCJhIjoiY2tldjkyOTJ1MDNxZzMxbjRqeDVsMDVhbSJ9.1kpoewjVxUM_9YOqpBaEeg`;
+
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
@@ -11,7 +13,7 @@ L.tileLayer(
     id: "mapbox/streets-v11",
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: APIKey,
+    accessToken: API_Key,
   }
 ).addTo(mymap);
 const marker = L.marker([lat, lng]).addTo(mymap);
